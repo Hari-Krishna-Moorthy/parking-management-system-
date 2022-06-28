2a7f5ac2aeb815b04b0f4b8f1b505d0535857cb8
Hari Krishna Moorthy <77602260+Hari-Krishna-Moorthy@users.noreply.github.com>
2022-06-28 13:31:48 +0530

    Add File via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './Components/Navbar';
+import DataBase from './Components/DataBase';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <DataBase />
+    </div>
+  );
+}
+
+export default App;
